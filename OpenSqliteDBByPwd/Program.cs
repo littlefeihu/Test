ab20790921bc81199904865720d82705b7c8612b
allen <378917466@qq.com>
2017-08-03 11:22:54 +0800

    打开加密的数据库

---
+++ OpenSqliteDBByPwd/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenSqliteDBByPwd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string DB_PATH = "Data Source=EncryptedDB.db3; Password=1111";
+
+
+            using (SQLiteConnection con = new SQLiteConnection(DB_PATH))
+            {
+                con.Open();
+                string sqlStr = @"INSERT INTO Customer(CUST_NO,CUSTOMER)
+                                  VALUES
+                                  (
+                                      3000,
+                                      'Allen'
+                                  )";
+                using (SQLiteCommand cmd = new SQLiteCommand(sqlStr, con))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
