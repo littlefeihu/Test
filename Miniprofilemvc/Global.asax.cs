25556b7e1dd47c68fb5883cb072bfce09f91f472
feihu <378917466@qq.com>
2017-08-12 22:19:21 +0800

    MiniProfiler

---
+++ Miniprofilemvc/Global.asax.cs
@@ -0,0 +1,36 @@
+﻿using StackExchange.Profiling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Miniprofilemvc
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_BeginRequest(Object source, EventArgs e)
+        {
+#if DEBUG
+            MiniProfiler.Start();
+#endif
+        }
+        protected void Application_EndRequest()
+        {
+#if DEBUG
+            MiniProfiler.Stop();
+#endif
+        }
+
+    }
+}
