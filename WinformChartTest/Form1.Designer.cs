6782d94f81d508bb3d0537f7454844d6fa85f6da
allen <378917466@qq.com>
2017-09-22 14:01:49 +0800

    winform chart图表控件的使用

--- WinformChartTest/Form1.Designer.cs
+++ WinformChartTest/Form1.Designer.cs
@@ -28,107 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint25 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint26 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(1D, 1D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint27 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(2D, 2D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint28 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(2.5D, 3D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint29 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(4D, 2D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint30 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(5D, 3D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint31 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(6D, -1D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint32 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(7D, -2D);
-            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint33 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 1D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint34 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(1D, -1D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint35 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(2D, -2D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint36 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(2.5D, -3D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint37 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(4D, -2D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint38 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(5D, -3D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint39 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(6D, -6D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint40 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(7D, -4D);
-            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint41 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, -1D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint42 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(1D, -5D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint43 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(2D, -6D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint44 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(2.5D, -7D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint45 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(4D, -8D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint46 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(5D, -9D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint47 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(6D, -5D);
-            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint48 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(7D, -9D);
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint1 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint2 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(1D, 1D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint3 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(2D, 2D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint4 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(2.5D, 3D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint5 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(4D, 2D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint6 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(5D, 3D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint7 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(6D, -1D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint8 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(7D, -2D);
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint9 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 1D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint10 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(1D, -1D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint11 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(2D, -2D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint12 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(2.5D, -3D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint13 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(4D, -2D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint14 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(5D, -3D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint15 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(6D, -6D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint16 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(7D, -4D);
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint17 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, -1D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint18 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(1D, -5D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint19 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(2D, -6D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint20 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(2.5D, -7D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint21 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(4D, -8D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint22 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(5D, -9D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint23 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(6D, -5D);
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint24 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(7D, -9D);
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
-            this.chart1.Location = new System.Drawing.Point(56, 34);
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 72);
             this.chart1.Name = "chart1";
             this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.EarthTones;
-            series4.ChartArea = "ChartArea1";
-            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series4.Legend = "Legend1";
-            series4.Name = "变量1";
-            series4.Points.Add(dataPoint25);
-            series4.Points.Add(dataPoint26);
-            series4.Points.Add(dataPoint27);
-            series4.Points.Add(dataPoint28);
-            series4.Points.Add(dataPoint29);
-            series4.Points.Add(dataPoint30);
-            series4.Points.Add(dataPoint31);
-            series4.Points.Add(dataPoint32);
-            series5.ChartArea = "ChartArea1";
-            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series5.Legend = "Legend1";
-            series5.Name = "变量2";
-            series5.Points.Add(dataPoint33);
-            series5.Points.Add(dataPoint34);
-            series5.Points.Add(dataPoint35);
-            series5.Points.Add(dataPoint36);
-            series5.Points.Add(dataPoint37);
-            series5.Points.Add(dataPoint38);
-            series5.Points.Add(dataPoint39);
-            series5.Points.Add(dataPoint40);
-            series6.ChartArea = "ChartArea1";
-            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series6.Legend = "Legend1";
-            series6.Name = "变量3";
-            series6.Points.Add(dataPoint41);
-            series6.Points.Add(dataPoint42);
-            series6.Points.Add(dataPoint43);
-            series6.Points.Add(dataPoint44);
-            series6.Points.Add(dataPoint45);
-            series6.Points.Add(dataPoint46);
-            series6.Points.Add(dataPoint47);
-            series6.Points.Add(dataPoint48);
-            this.chart1.Series.Add(series4);
-            this.chart1.Series.Add(series5);
-            this.chart1.Series.Add(series6);
-            this.chart1.Size = new System.Drawing.Size(852, 410);
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.IsValueShownAsLabel = true;
+            series1.Label = "#SERIESNAME;X:#VALX，Y：#VALY";
+            series1.Legend = "Legend1";
+            series1.Name = "变量1";
+            series1.Points.Add(dataPoint1);
+            series1.Points.Add(dataPoint2);
+            series1.Points.Add(dataPoint3);
+            series1.Points.Add(dataPoint4);
+            series1.Points.Add(dataPoint5);
+            series1.Points.Add(dataPoint6);
+            series1.Points.Add(dataPoint7);
+            series1.Points.Add(dataPoint8);
+            series1.SmartLabelStyle.CalloutBackColor = System.Drawing.SystemColors.ActiveCaption;
+            series1.ToolTip = "#LABEL#VAL;#VALY";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "变量2";
+            series2.Points.Add(dataPoint9);
+            series2.Points.Add(dataPoint10);
+            series2.Points.Add(dataPoint11);
+            series2.Points.Add(dataPoint12);
+            series2.Points.Add(dataPoint13);
+            series2.Points.Add(dataPoint14);
+            series2.Points.Add(dataPoint15);
+            series2.Points.Add(dataPoint16);
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "变量3";
+            series3.Points.Add(dataPoint17);
+            series3.Points.Add(dataPoint18);
+            series3.Points.Add(dataPoint19);
+            series3.Points.Add(dataPoint20);
+            series3.Points.Add(dataPoint21);
+            series3.Points.Add(dataPoint22);
+            series3.Points.Add(dataPoint23);
+            series3.Points.Add(dataPoint24);
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(1290, 612);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(926, 109);
+            this.button1.Location = new System.Drawing.Point(900, 25);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "生成图片";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "导入Excel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1010, 468);
+            this.ClientSize = new System.Drawing.Size(1314, 696);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.chart1);
             this.Name = "Form1";
@@ -142,6 +163,8 @@
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- WinformChartTest/Form1.cs
+++ WinformChartTest/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,31 +17,37 @@ namespace WinformChartTest
         public Form1()
         {
             InitializeComponent();
-            this.chart1.Series[0].MarkerStyle = MarkerStyle.Triangle;
-            this.chart1.Series[1].MarkerStyle = MarkerStyle.Circle;
-            this.chart1.Series[2].MarkerStyle = MarkerStyle.Square;
-            //this.chart1.ChartAreas[0].AxisX.IsStartedFromZero = true;
-            //this.chart1.ChartAreas[0].AxisY.IsStartedFromZero = true;
+            //this.chart1.Series[0].MarkerStyle = MarkerStyle.Triangle;
+            //this.chart1.Series[1].MarkerStyle = MarkerStyle.Circle;
+            //this.chart1.Series[2].MarkerStyle = MarkerStyle.Square;
+            ////this.chart1.ChartAreas[0].AxisX.IsStartedFromZero = true;
+            ////this.chart1.ChartAreas[0].AxisY.IsStartedFromZero = true;
 
-            this.chart1.Series[0].MarkerSize = 8;
-            this.chart1.Series[1].MarkerSize = 8;
-            this.chart1.Series[2].MarkerSize = 8;
+            //this.chart1.Series[0].MarkerSize = 8;
+            //this.chart1.Series[1].MarkerSize = 8;
+            //this.chart1.Series[2].MarkerSize = 8;
 
 
-            this.chart1.Series[0].MarkerColor = Color.Black;
-            this.chart1.Series[1].MarkerColor = Color.Blue;
-            this.chart1.Series[2].MarkerColor = Color.Orange;
-            this.chart1.MouseClick += (sender, ex) =>
-            {
-                var HPoint = chart1.HitTest(ex.X, ex.Y);
-                if (HPoint.PointIndex == -1 || HPoint.Series == null) return;
+            //this.chart1.Series[0].MarkerColor = Color.Black;
+            //this.chart1.Series[1].MarkerColor = Color.Blue;
+            //this.chart1.Series[2].MarkerColor = Color.Orange;
+            //this.chart1.MouseClick += (sender, ex) =>
+            //{
+            //    var HPoint = chart1.HitTest(ex.X, ex.Y);
+            //    if (HPoint.PointIndex == -1 || HPoint.Series == null) return;
 
-                HPoint.Series.Points[HPoint.PointIndex].LabelBackColor = Color.YellowGreen;
-                if (HPoint.Series.Points[HPoint.PointIndex].Label == string.Empty)
-                {
-                    HPoint.Series.Points[HPoint.PointIndex].Label = HPoint.Series.Name + "(" + HPoint.Series.Points[HPoint.PointIndex].YValues[0].ToString() + ":" + HPoint.Series.Points[HPoint.PointIndex].XValue.ToString() + ")";
-                }
-            };
+            //    HPoint.Series.Points[HPoint.PointIndex].LabelBackColor = Color.YellowGreen;
+            //    if (HPoint.Series.Points[HPoint.PointIndex].Label == string.Empty)
+            //    {
+            //        HPoint.Series.Points[HPoint.PointIndex].Label = HPoint.Series.Name + "(" + HPoint.Series.Points[HPoint.PointIndex].YValues[0].ToString() + ":" + HPoint.Series.Points[HPoint.PointIndex].XValue.ToString() + ")";
+            //    }
+            //};
+
+            //var p1 = new DataPoint();
+            //p1.SetValueXY(10, 10);
+            //p1.Label = "dd";
+            //p1.LabelBackColor = Color.YellowGreen;
+            //chart1.Series[0].Points.Add(p1);
 
         }
 
@@ -48,6 +55,55 @@ namespace WinformChartTest
         {
 
             chart1.SaveImage("ddd.png", System.Windows.Forms.DataVisualization.Charting.ChartImageFormat.Png);
+        }
+
+        List<string> XData = new List<string>();
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var fName = openFileDialog1.FileName;
+                var dt = NPOIHelper.Import(fName);
+
+                this.chart1.Series.Clear();
+
+                Random random = new Random();
+
+                int columnIndex = 0;
+                foreach (DataColumn column in dt.Columns)
+                {
+                    if (column.ColumnName != "卡片")
+                    {
+                        var seriesName = column.ColumnName;
+                        var series = new Series();
+                        series.ChartType = SeriesChartType.Line;
+                        series.Name = seriesName;
+                        this.chart1.Series.Add(series);
+                        int rowIndex = 0;
+
+                        foreach (DataRow row in dt.Rows)
+                        {
+                            var x = row["卡片"];
+                            var y = row[seriesName];
+                            var p1 = new DataPoint();
+                            p1.SetValueXY(x, y);
+
+                            //设置显示Label信息
+                            if (rowIndex == columnIndex + 5 && (columnIndex == 2 || columnIndex == 4) ||
+                                ((rowIndex == dt.Rows.Count - 10) && (columnIndex == 2 || columnIndex == 4))
+                                || ((rowIndex == dt.Rows.Count / 2) && (columnIndex == 1 || columnIndex == 3)))
+                            {
+                                p1.Label = "\r\n" + seriesName + "\r\n 时间：" + x + "\r\n温度：" + y + "\r\n";
+                                p1.LabelBackColor = Color.YellowGreen;
+                            }
+                            series.Points.Add(p1);
+                            rowIndex += 1;
+                        }
+                    }
+                    columnIndex += 1;
+                }
+            }
         }
     }
 }
