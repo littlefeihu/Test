d43fecaae853f45214cc2e8b50b6641b7dd12ba5
allen <378917466@qq.com>
2017-10-09 16:15:16 +0800

    加载word文档

--- DSOFramerTest/Form1.Designer.cs
+++ DSOFramerTest/Form1.Designer.cs
@@ -32,9 +32,13 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.axFramerControl1 = new AxDSOFramer.AxFramerControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.axFramerControl1)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -42,45 +46,77 @@
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.2311F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 88.7689F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(722, 463);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(995, 635);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.axFramerControl1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(3, 54);
+            this.panel1.Location = new System.Drawing.Point(3, 74);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(716, 406);
+            this.panel1.Size = new System.Drawing.Size(989, 558);
             this.panel1.TabIndex = 0;
             // 
             // axFramerControl1
             // 
+            this.axFramerControl1.AllowDrop = true;
             this.axFramerControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.axFramerControl1.Enabled = true;
             this.axFramerControl1.Location = new System.Drawing.Point(0, 0);
             this.axFramerControl1.Name = "axFramerControl1";
             this.axFramerControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axFramerControl1.OcxState")));
-            this.axFramerControl1.Size = new System.Drawing.Size(716, 406);
+            this.axFramerControl1.Size = new System.Drawing.Size(989, 558);
             this.axFramerControl1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "添加书签";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(989, 65);
+            this.panel2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(248, 8);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(722, 463);
+            this.ClientSize = new System.Drawing.Size(995, 635);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.axFramerControl1)).EndInit();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -90,6 +126,9 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Panel panel1;
         private AxDSOFramer.AxFramerControl axFramerControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- DSOFramerTest/Form1.cs
+++ DSOFramerTest/Form1.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Word;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,16 +16,145 @@ namespace DSOFramerTest
 {
     public partial class Form1 : Form
     {
+        //鼠标按下坐标（control控件的相对坐标）
+        Point mouseDownPoint = Point.Empty;
+        //显示拖动效果的矩形
+        System.Drawing.Rectangle rect = System.Drawing.Rectangle.Empty;
+        //是否正在拖拽
+        bool isDrag = false;
         public Form1()
         {
             InitializeComponent();
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormDrag_Paint);
             var wordtemplate = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "冷库验证报告模板.doc");
             axFramerControl1.Titlebar = false;
             axFramerControl1.Menubar = false;
-            axFramerControl1.Toolbars = false;
+            axFramerControl1.Toolbars = true;
             axFramerControl1.Open(wordtemplate);
+            this.SizeChanged += Form1_SizeChanged;
+            //button2.MouseDown += new MouseEventHandler(control_MouseDown);
+            //button2.MouseMove += new MouseEventHandler(control_MouseMove);
+            //button2.MouseUp += new MouseEventHandler(control_MouseUp);
+            this.FormClosed += Form1_FormClosed;
         }
 
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            axFramerControl1.Refresh();
+        }
 
+        void control_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseDownPoint = e.Location;
+                //记录控件的大小
+                rect = button2.Bounds;
+            }
+        }
+        void control_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrag = true;
+                //重新设置rect的位置，跟随鼠标移动
+                rect.Location = getPointToForm(new Point(e.Location.X - mouseDownPoint.X, e.Location.Y - mouseDownPoint.Y));
+                this.Refresh();
+
+            }
+        }
+        void control_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (isDrag)
+                {
+                    isDrag = false;
+                    //移动control到放开鼠标的地方
+                    button2.Location = rect.Location;
+                    this.Refresh();
+                }
+                reset();
+            }
+        }
+        //重置变量
+        private void reset()
+        {
+            mouseDownPoint = Point.Empty;
+            rect = System.Drawing.Rectangle.Empty;
+            isDrag = false;
+        }
+        //窗体重绘
+        private void FormDrag_Paint(object sender, PaintEventArgs e)
+        {
+            if (rect != System.Drawing.Rectangle.Empty)
+            {
+                if (isDrag)
+                {//画一个和Control一样大小的黑框
+                    e.Graphics.DrawRectangle(Pens.Black, rect);
+                }
+                else
+                {
+                    e.Graphics.DrawRectangle(new Pen(this.BackColor), rect);
+                }
+            }
+        }
+        //把相对与control控件的坐标，转换成相对于窗体的坐标。
+        private Point getPointToForm(Point p)
+        {
+            return this.PointToClient(button2.PointToScreen(p));
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            axFramerControl1.Close();
+
+            axFramerControl1.Dispose();
+        }
+        //第一步：注册AxFramerControl
+        public bool RegControl()
+        {
+            try
+            {
+
+                Assembly thisExe = Assembly.GetExecutingAssembly();
+                System.IO.Stream myS = thisExe.GetManifestResourceStream("NameSpaceName.dsoframer.ocx");
+
+                string sPath = System.AppDomain.CurrentDomain.BaseDirectory + @"\dsoframer.ocx";
+                ProcessStartInfo psi = new ProcessStartInfo("regsvr32", "/s " + sPath);
+                Process.Start(psi);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            DocumentClass wordDoc = axFramerControl1.ActiveDocument as DocumentClass;
+
+            Microsoft.Office.Interop.Word.Application wordApp = wordDoc.Application;
+
+            Microsoft.Office.Interop.Word.Range r = wordDoc.Paragraphs[15].Range;
+
+            wordApp.Selection.Bookmarks.Add("ddee1");
+
+            wordApp.Selection.Font.Color = WdColor.wdColorRed;
+            wordApp.Selection.TypeText("ddee1");
+
+            axFramerControl1.Save();
+
+        }
+
+        ///// <summary>
+        ///// 替换标签下
+        ///// </summary>
+        ///// <param name="_sMark"></param>
+        ///// <param name="_sReplaceText"></param>
+        ///// <param name="_IsD">替换后是否突出显示</param>
+        ///// <returns></returns>
+        //public bool WordReplace(string _sMark, string _sReplaceText, bool _IsD)
     }
 }
